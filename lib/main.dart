b90d75c96835ee4c88da803dde64a95c864cd1ed
giuseppe <48530807+giuseppemacc@users.noreply.github.com>
2020-07-28 20:42:12 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import './MainPage.dart';
 
-void main() => runApp(new Main());
+void main() {
+  runApp(new Main());
+}
 
 class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Automatic Test 2",
+      title: "Automatic Test",
       theme: ThemeData(primarySwatch: Colors.green),
       home: SafeArea(child: MainPage()),
     );
