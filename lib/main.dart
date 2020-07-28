def933cbccba432753baaac62484d2fe86461de0
giuseppe <48530807+giuseppemacc@users.noreply.github.com>
2020-07-28 20:55:53 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,11 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< HEAD
       title: "Automatic asd 2",
+=======
+      title: "Automatic Test",
+>>>>>>> parent of f9c4d0b... Update main.dart
       theme: ThemeData(primarySwatch: Colors.green),
       home: SafeArea(child: MainPage()),
     );
